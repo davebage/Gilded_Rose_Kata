e456c62db01cf46e17ede32f95894c0a843ac2cf
Dave Bage <david_bage@hotmail.com>
2022-10-26 10:52:40 +0100

    Added Mongoose Elixir coverage test

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -7,12 +7,14 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void Update_Quality_For_Mongoose_Elixir_Reduce_Sell_In_And_Quality_By_One()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("foo", Items[0].Name);
+            Assert.AreEqual("Elixir of the Mongoose", Items[0].Name);
+            Assert.AreEqual(4, Items[0].SellIn);
+            Assert.AreEqual(6, Items[0].Quality);
         }
     }
 }
