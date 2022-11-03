d8603746b78a4cd2eede513f9c6d8424e3e4e262
jeeva <jeeva.ramu@codurance.com>
2022-11-03 16:25:52 +0000

    created aged brie encapsulation

---
+++ AgedBrieItemProcess.cs
@@ -0,0 +1,11 @@
+namespace csharp
+{
+    public class AgedBrieItemProcess
+    {
+        public void IncreaseItemQuality(Item item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
